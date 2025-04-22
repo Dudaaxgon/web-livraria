8bba2997dc356d0afa26fc2ae0fcbac16fe501be
Dudaaxgon <dudapgoncalves@gmail.com>
2025-04-21 21:56:35 -0300

    update

--- script.js
+++ script.js
@@ -5,16 +5,3 @@ function clickMenu() {
         menu.style.display = 'block';
     }
 }
-
-// Caixinhas da section categorias
-const caixinhas = document.querySelectorAll(".caixinha");
-
-caixinhas.forEach(function(caixinha) {
-  caixinha.addEventListener("mouseenter", function() {
-    caixinha.style.background = "#55130b";
-  });
-
-  caixinha.addEventListener("mouseout", function() {
-    caixinha.style.background = "#7e6d5f";
-  });
-});
